cc4458d584d98cd9e04b4d631183783557308fd3
nvarghese-u2can <84356761+nvarghese-u2can@users.noreply.github.com>
2021-05-17 21:20:26 -0400

    Add files via upload
    String interpolation - .format and f-strings methods

---
+++ str_interpolation.format.py
@@ -0,0 +1,12 @@
+print("The {q} {b} {f}".format(b='Brown', f='fox', q='quick'))
+
+# float-formatting example
+data = 100 / 3
+print("The data is {d:1.3f}".format(d=data))
+
+# f-strings method ( Formatted String Literals )
+
+w1 = "Wonderful"
+w2 = "World"
+
+print(f'It is a {w1} {w2}!')
